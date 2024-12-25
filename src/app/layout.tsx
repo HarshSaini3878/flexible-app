fbd653d54bdd69b49f3490e78481e211aee5a925
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-12-25 22:24:45 +0530

    changed scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className='relative'>
+      <body className='relative scroll-container '>
         <NavBar />
         {children}
         <Footer />
